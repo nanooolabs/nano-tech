a812dcece1a2ecdcf5ce9cf9d0a3c149c383a943
Paras Bokhari <parasbokhari@gmail.com>
2024-08-07 01:47:26 -0500

    limiting options in code field

--- sanity/schemaTypes/blocks/defaultFields.js
+++ sanity/schemaTypes/blocks/defaultFields.js
@@ -2,5 +2,9 @@ export const scopedCss = {
   name: "scoped_css",
   title: "Scoped CSS",
   type: "code",
+  options: {
+    language: "scss",
+    languageAlternatives: [{ title: "SCSS", value: "scss" }],
+  },
   group: "style",
 };
