f7ec2d3f519a4c07f14d802eccc4e0920eec4b30
Paras Bokhari <parasbokhari@gmail.com>
2024-08-08 01:46:49 -0500

    checking project prod url

--- app/(frontend)/layout.js
+++ app/(frontend)/layout.js
@@ -15,7 +15,11 @@ export const customFont = localFont({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={customFont.variable}>
+      <body
+        data-vercel-url={NEXT_PUBLIC_VERCEL_URL}
+        data-vercel-prod-url={NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}
+        className={customFont.variable}
+      >
         <NextTopLoader
           color="var(--t-primary-branding-color)"
           showSpinner={false}
