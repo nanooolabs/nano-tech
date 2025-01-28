dc53b9351e2e727218bc497a32c088b3ce79a4de
ali-hassnain9 <alihassnain5@gmail.com>
2025-01-29 01:51:41 +0400

    check with different api version

--- sanity/config/clientConfig.js
+++ sanity/config/clientConfig.js
@@ -4,7 +4,7 @@ const clientConfig = {
   projectId: projectId,
   dataset: "production",
   useCdn: false,
-  apiVersion: "2024-07-31",
+  apiVersion: '2023-08-01's,
   stega: {
     enabled: false,
     studioUrl: "/studio",
